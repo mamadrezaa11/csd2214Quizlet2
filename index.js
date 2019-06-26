1e538d8170727b86ae5c3d5670759e57f1dbff51
mamadrezaa11 <50425906+mamadrezaa11@users.noreply.github.com>
2019-06-26 14:15:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-//challenge
-//create a function "waitinFn"
-waitingFn
+function waitingFn(TimeInMs){
+    const futureTime=Date.now() = TimeInMs;
+    while (futureTime > Date.now()){
+        // waiting
+        
+    }
+}
+waitingFn(3000);
+console.log("function called has just ended");
